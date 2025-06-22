907aae116a47f32e42142837a4659c8040329691
krutika nikhade <krutikanikhade0204@gmail.com>
2025-06-22 12:47:50 +0530

    Added Steps and Description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
+import Steps from "../components/Steps";
+import Description from '../components/Description';
 
 const Home = () => {
   return (
-    <div></div>
+    <div>
+
+      <Header/>
+      <Steps/>
+      <Description/>
+    </div>
   )
 }
 
-export default Home
+export default Home;
